45882fbd2f4857f184cb237fdd061edea1e0a883
Linda <m4rn0ta@gmail.com>
2021-11-28 17:02:55 +0100

    sachy

---
+++ week 1/day 3/src/DrawChess.java
@@ -0,0 +1,21 @@
+package src;
+
+import java.util.Scanner;
+
+public class DrawChess {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How big chess should be? :");
+        int c = scanner.nextInt();
+        for (int a = 0; a < c; a++) {
+            for (int b = 0; b < c; b++) {
+                if (a % 2 != 0) {              //pokud se rovná 2, budou řádky prohozeny
+                    System.out.print(" %");
+                } else {
+                    System.out.print("% ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
