254d953517fe453a21a4cf07383d3936048d9139
Linda <m4rn0ta@gmail.com>
2021-11-28 21:25:25 +0100

    pridany barvy do pole

---
+++ week 1/day 4/src/Colores.java
@@ -0,0 +1,14 @@
+public class Colores {
+    public static void main(String[] args) {
+
+        String[][] colors = {
+
+                {"lime", "forest green", "olive", "pale green", "spring green"},
+                {"orange red", "red", "tomato"},
+                {"orchid", "violet", "pink", "hot pink"}
+        };
+
+
+
+    }
+}
