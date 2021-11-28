121747127677a9cb74656ffe1ab3937a764d356d
Linda <m4rn0ta@gmail.com>
2021-11-28 17:11:39 +0100

    drobna uprava v sachach

--- week 1/day 3/src/DrawChess.java
+++ week 1/day 3/src/DrawChess.java
@@ -9,7 +9,7 @@ public class DrawChess {
         int c = scanner.nextInt();
         for (int a = 0; a < c; a++) {
             for (int b = 0; b < c; b++) {
-                if (a % 2 != 0) {              //pokud se rovná 2, budou řádky prohozeny
+                if (a % 2 != 0) {              //pokud se rovna 2, budou řádky prohozeny
                     System.out.print(" %");
                 } else {
                     System.out.print("% ");
