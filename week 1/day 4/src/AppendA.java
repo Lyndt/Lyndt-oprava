bd1a26bc05c6be71fb2aac1ccb0da877614de1d5
Linda <m4rn0ta@gmail.com>
2021-11-28 21:34:50 +0100

    pridavani hodnoty do soutu

--- week 1/day 4/src/AppendA.java
+++ week 1/day 4/src/AppendA.java
@@ -10,3 +10,9 @@ public class AppendA {
         }
     }
 }
+// - Create an array variable named `animals`
+//   with the following content:
+//   `["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill",
+//     "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "pirahn"]`
+//
+// - Add an `"a"` at the end of all elements (use a loop!)
