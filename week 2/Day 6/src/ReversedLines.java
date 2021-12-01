78e3f07d56ddc77db43199975bcb1ef6a5977eff
Linda <m4rn0ta@gmail.com>
2021-12-01 12:20:23 +0100

    prejmenovani souboru

---
+++ week 2/Day 6/src/ReversedLines.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReversedLines {
+    public static void main(String[] IOException) throws Exception {
+        Path ReversedLines = Paths.get("week 2/Day 6/ReversedLines.txt");
+        Files.readAllLines()
+    }
+    public static void file(String
+    }
+
+}
+// Create a method that decrypts reversed-lines.txt
