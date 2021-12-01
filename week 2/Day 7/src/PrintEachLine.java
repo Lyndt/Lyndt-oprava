29f63acfd7d7ff26535b8dc8311493eed27aa306
Linda <m4rn0ta@gmail.com>
2021-12-01 14:33:36 +0100

    vyzkousela jsem jestli nacte txt a pak jsem ho prejmenovala..

--- week 2/Day 7/src/PrintEachLine.java
+++ week 2/Day 7/src/PrintEachLine.java
@@ -1,11 +1,29 @@
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrintEachLine {
     public static void main(String[] args) {
-        Path path = Paths.get()
-        Files.readAllLines()
+        Path path = Paths.get("my-file.txt");
+        try {
+            Files.readAllLines(path);
+            List<String> lines = Files.readAllLines(path);
+            for (String line : lines) {
+                System.out.println(line);
+
+            }
+        }
+        catch (IOException chyba) {
+            System.out.println("not founded .txt");
+        }
+
+
+
+
+
 
     }
 }
