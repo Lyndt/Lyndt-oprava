90900a09d188b63c8b4dc99a9700634397b9f4e0
Linda <m4rn0ta@gmail.com>
2021-12-01 13:46:32 +0100

    pridan .txt

---
+++ week 2/Day 7/src/PrintEachLine.java
@@ -0,0 +1,15 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PrintEachLine {
+    public static void main(String[] args) {
+        Path path = Paths.get()
+        Files.readAllLines()
+
+    }
+}
+// Write a program that opens a file called "my-file.txt" and prints each
+// line from the file.
+// If the program is unable to read the file (for example the file does not exist),
+// it should print the following error message: "Unable to read file: my-file.txt".
