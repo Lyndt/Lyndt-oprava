813575d4a32e8f66eda84bf393bc9fbf87374f2a
Linda <m4rn0ta@gmail.com>
2021-12-03 15:38:50 +0100

    LinePlay dokoncen, mela jsem chybu v loopu

--- week 2/Day 05/Drawing project/src/LinePlay.java
+++ week 2/Day 05/Drawing project/src/LinePlay.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class LinePlay {
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class LinePlay {
+    public static void drawImage(Graphics graphics){
+        // draw a red horizontal line to the center of the canvas
+        // draw a green vertical line to the center of the canvas
+
+        for (int i = 25; i <WIDTH; i+= 25) {
+            graphics.setColor(Color.green);
+            graphics.drawLine(0, i, i,HEIGHT);
+        }
+        for (int j = 25; j <WIDTH; j+= 25) {
+            graphics.setColor(new Color(178,102,255));
+            graphics.drawLine(j, 0, WIDTH, j);
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 500;
+    static int HEIGHT = 500;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            drawImage(graphics);
+        }
+    }
 }
