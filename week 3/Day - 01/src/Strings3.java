7e474b2c879c60df2359551e03abd0d8550ffc25
Linda <m4rn0ta@gmail.com>
2021-12-06 15:59:30 +0100

    not done yet

---
+++ week 3/Day - 01/src/Strings3.java
@@ -0,0 +1,9 @@
+public class Strings3 {
+    public static void main(String[] args) {
+
+    }
+}
+// Given a string, write a recursive (no loops) method which returns a new
+// string with all adjacent chars separated by an `*`. Please note that there
+// should be no trailing `*` at the end of the returned string. For example,
+// the string `word` should be changed to `w*o*r*d`.
