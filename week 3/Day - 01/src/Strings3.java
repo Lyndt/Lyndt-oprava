1943ca41933d85c28bbf887470b9a8daf4854bfe
Linda <m4rn0ta@gmail.com>
2021-12-06 16:20:27 +0100

    Strings3 done

--- week 3/Day - 01/src/Strings3.java
+++ week 3/Day - 01/src/Strings3.java
@@ -1,7 +1,31 @@
+import java.io.OutputStream;
+
 public class Strings3 {
     public static void main(String[] args) {
+        System.out.println(stringWithStars("Hello world"));
+//        String input = "hello";
+//        pairStars(input, 0);
+//        System.out.println(OutputStream.nullOutputStream());
 
+//    }
+//    static String output;
+//    public static void pairStars(String input, int i) {
+//        output = output + input.charAt(i);
+//        if (i == input.length() - 1) {
+//            return;
+//        }
+//        if (input.charAt(i) == input.charAt(i + 1)) {
+//            output = output + '*';
+//        }
+//         pairStars(input, i + 1);
     }
+    public static String stringWithStars(String str)
+    {
+        if(str.length() < 2)
+            return str;
+        return str.charAt(0) + "*" + stringWithStars(str.substring(1));
+    }
+
 }
 // Given a string, write a recursive (no loops) method which returns a new
 // string with all adjacent chars separated by an `*`. Please note that there
