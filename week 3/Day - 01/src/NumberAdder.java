eba40726a1b6f607ee922febb91721fbe6f7f5f8
Linda <m4rn0ta@gmail.com>
2021-12-06 11:16:51 +0100

    NumberAdder done

---
+++ week 3/Day - 01/src/NumberAdder.java
@@ -0,0 +1,19 @@
+public class NumberAdder {
+    public static void main(String[] args) {
+        System.out.println(recursiveN(10));
+
+    }
+    public static int recursiveN(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        else {
+            return n + recursiveN(n - 1);
+        }
+
+    }
+
+}
+// Write a recursive function that takes one parameter: n and returns
+// the sum of numbers (integers) from 1 to n. The function should
+// return 0 for inputs less than 1.
