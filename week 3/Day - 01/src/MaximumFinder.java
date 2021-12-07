1923c50ec6da0eeac1bbe5741c19778b3d0dcdde
Linda <m4rn0ta@gmail.com>
2021-12-07 12:20:15 +0100

    added week 3, day 2

---
+++ week 3/Day - 01/src/MaximumFinder.java
@@ -0,0 +1,10 @@
+public class MaximumFinder {
+    public static void main(String[] args) {
+
+    }
+    public static int largestElement(int largestNum) {
+    }
+
+
+}
+// Write a function which returns the largest element of an array using recursion.
