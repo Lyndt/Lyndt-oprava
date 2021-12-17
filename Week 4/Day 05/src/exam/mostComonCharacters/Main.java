11292b83134828ab84495a9eac8db30682ccd66a
Linda <m4rn0ta@gmail.com>
2021-12-17 11:07:42 +0100

    MostCommonCharacters starting again

--- Week 4/Day 05/src/exam/mostComonCharacters/Main.java
+++ Week 4/Day 05/src/exam/mostComonCharacters/Main.java
@@ -8,17 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        readFromFile();
 
-    }
-    public static void readFromFile() throws IOException {
-        Path path = Paths.get("countchar.txt");
-        List<String> file = new ArrayList<>();
-        file = Files.readAllLines(path);
-        for (String files : file) {
-            System.out.println(files);
-        }
-    }
 
 }
