248686bc1f67d7de7cf0be0df792542caed8dc29
Linda <m4rn0ta@gmail.com>
2022-01-04 09:40:29 +0100

    SimpleReplace2 done

---
+++ week 2/Day 6/src/ShoppingList.java
@@ -0,0 +1,18 @@
+public class ShoppingList {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+/*    We are going to represent a shopping list by a list containing strings.
+
+        Create a list with the following items:
+        eggs, milk, fish, apples, bread and chicken
+        Create an application which prints out the answers to the following questions:
+        Do we have milk in the shopping list? (yes/no)
+        Do we have bananas in the shopping list? (yes/no)
+        The full output of your main method should be the following:
+
+        yes
+        no */
---
+++ week 2/Day 6/src/SimpleReplace2.java
@@ -0,0 +1,15 @@
+public class SimpleReplace2 {
+    public static void main(String... args){
+        String example = "In a dishwasher far, far away";
+
+        String newExample = example.replace("dishwasher", "galaxy");
+
+        // I would like to replace "dishwasher" with "galaxy" in this `example` variable
+        // but there is an issue in the code above
+        // Please fix it for me!
+
+        // Expected output: In a galaxy far, far away
+        System.out.println(example);
+        System.out.println(newExample);
+    }
+}
