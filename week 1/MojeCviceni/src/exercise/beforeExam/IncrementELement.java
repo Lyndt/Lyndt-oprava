49d0e4544abe49f24141376e5cb6afb4268df1ec
Linda <m4rn0ta@gmail.com>
2022-01-05 13:23:13 +0100

    IncrementElement done

---
+++ week 1/MojeCviceni/src/exercise/beforeExam/IncrementELement.java
@@ -0,0 +1,13 @@
+package exercise.beforeExam;
+
+public class IncrementELement {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        numbers[2]++;
+        System.out.println(numbers[2]);
+    }
+}
+// - Create an array variable named `numbers`
+//   with the following content: `[1, 2, 3, 4, 5]`
+// - Increment the third element
+// - Print the third element
