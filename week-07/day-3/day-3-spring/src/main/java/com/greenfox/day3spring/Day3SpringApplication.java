27cc662a9fabc97f0a41d5dcc1b58c7eceac85d3
Linda <m4rn0ta@gmail.com>
2022-01-19 09:27:53 +0100

    week-07 day-3 added SpringDemo

---
+++ week-07/day-3/day-3-spring/src/main/java/com/greenfox/day3spring/Day3SpringApplication.java
@@ -0,0 +1,13 @@
+package com.greenfox.day3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3SpringApplication.class, args);
+    }
+
+}
